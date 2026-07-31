65da04cd808750e53fb47da400ecc0ac52fce532
Pitabash Choudhdry <pitabash.c@inspironlabs.com>
2026-07-31 18:21:13 +0530

    button group component design in progress, over all component design , need to handle dynamic functionality callback and text nummbers

--- app/src/main/java/com/social/connectMe/ui/components/buttons/ButtonWithLabel.kt
+++ app/src/main/java/com/social/connectMe/ui/components/buttons/ButtonWithLabel.kt
@@ -1,13 +1,16 @@
 package com.social.connectMe.ui.components.buttons
 
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ButtonWithLabel(
@@ -15,8 +18,14 @@ fun ButtonWithLabel(
     logo: ImageVector
 ) {
 
-    Row() {
-        Icon(logo, contentDescription = "Like")
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(logo, contentDescription = "Like",
+            tint = androidx.compose.ui.graphics.Color.Black,
+            modifier = Modifier.size(30.dp)
+            )
         Text(label)
     }
 
--- app/src/main/java/com/social/connectMe/ui/components/content/ContentBottomSection.kt
+++ app/src/main/java/com/social/connectMe/ui/components/content/ContentBottomSection.kt
@@ -2,16 +2,24 @@ package com.social.connectMe.ui.components.content
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.social.connectMe.ui.components.buttons.ButtonWithLabel
 
 @Composable
 fun ContentBottomSection(){
 
-    Column(){
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .padding(1.dp,5.dp,5.dp,1.dp)
+
+    ){
        // ContentLikeSection()
 //        ContentCommentSection()
 //        ContentShareSection()
